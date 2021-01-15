bbf77067fb0946a2422a2718668affd081d3d4eb
Abdullah <hungrytoast99@gmail.com>
2021-01-15 21:33:21 +0200

    Player Movement Script
    player movement scrip and controller input layout are saved under a file called scripts under my file

---
+++ Assets/Abdullah/Scripts/Controller.cs
@@ -0,0 +1,263 @@
+// GENERATED AUTOMATICALLY FROM 'Assets/Abdullah/Scripts/Controller.inputactions'
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.Utilities;
+
+public class @Controller : IInputActionCollection, IDisposable
+{
+    public InputActionAsset asset { get; }
+    public @Controller()
+    {
+        asset = InputActionAsset.FromJson(@"{
+    ""name"": ""Controller"",
+    ""maps"": [
+        {
+            ""name"": ""Player"",
+            ""id"": ""18ed9aa0-66cb-4a16-8e1d-837f3d1680d1"",
+            ""actions"": [
+                {
+                    ""name"": ""Movement"",
+                    ""type"": ""PassThrough"",
+                    ""id"": ""b8228302-1c86-4e08-af05-88241b1a8e9c"",
+                    ""expectedControlType"": ""Vector2"",
+                    ""processors"": """",
+                    ""interactions"": """"
+                },
+                {
+                    ""name"": ""Jump"",
+                    ""type"": ""Button"",
+                    ""id"": ""5c517d44-8d6f-45b3-957f-cd56c7320f5a"",
+                    ""expectedControlType"": ""Button"",
+                    ""processors"": """",
+                    ""interactions"": """"
+                },
+                {
+                    ""name"": ""Look"",
+                    ""type"": ""PassThrough"",
+                    ""id"": ""b7a6a53f-82ed-4471-a414-771b8e7b1c5b"",
+                    ""expectedControlType"": ""Vector2"",
+                    ""processors"": """",
+                    ""interactions"": """"
+                },
+                {
+                    ""name"": ""Rin"",
+                    ""type"": ""Button"",
+                    ""id"": ""919c190c-3357-40cf-9ef0-b6d59b807e0d"",
+                    ""expectedControlType"": ""Button"",
+                    ""processors"": """",
+                    ""interactions"": """"
+                }
+            ],
+            ""bindings"": [
+                {
+                    ""name"": ""WASD"",
+                    ""id"": ""cfacdf24-8ef4-4ee8-a43a-f624589d9804"",
+                    ""path"": ""2DVector"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Movement"",
+                    ""isComposite"": true,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": ""up"",
+                    ""id"": ""8e78fca9-bbda-49a0-9d2b-c749a0a2abbc"",
+                    ""path"": ""<Keyboard>/w"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Movement"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": ""down"",
+                    ""id"": ""7dc5a82e-7cd0-4cea-8b31-b7150b7aae99"",
+                    ""path"": ""<Keyboard>/s"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Movement"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": ""left"",
+                    ""id"": ""93ffb390-9724-4081-b632-d8ddd8fd2865"",
+                    ""path"": ""<Keyboard>/a"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Movement"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": ""right"",
+                    ""id"": ""4e710641-e640-4b6f-9f39-1d31f3cf8b34"",
+                    ""path"": ""<Keyboard>/d"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Movement"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": """",
+                    ""id"": ""73765232-c756-4cd1-ba37-917cda663eae"",
+                    ""path"": ""<Keyboard>/space"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Jump"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": """",
+                    ""id"": ""df14e466-f2a8-433c-9c63-3d4c50dbf575"",
+                    ""path"": ""<Mouse>/delta"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Look"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": """",
+                    ""id"": ""791f43b8-185c-4d52-998d-319947fd1b95"",
+                    ""path"": ""<Keyboard>/shift"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Rin"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
+                }
+            ]
+        }
+    ],
+    ""controlSchemes"": []
+}");
+        // Player
+        m_Player = asset.FindActionMap("Player", throwIfNotFound: true);
+        m_Player_Movement = m_Player.FindAction("Movement", throwIfNotFound: true);
+        m_Player_Jump = m_Player.FindAction("Jump", throwIfNotFound: true);
+        m_Player_Look = m_Player.FindAction("Look", throwIfNotFound: true);
+        m_Player_Rin = m_Player.FindAction("Rin", throwIfNotFound: true);
+    }
+
+    public void Dispose()
+    {
+        UnityEngine.Object.Destroy(asset);
+    }
+
+    public InputBinding? bindingMask
+    {
+        get => asset.bindingMask;
+        set => asset.bindingMask = value;
+    }
+
+    public ReadOnlyArray<InputDevice>? devices
+    {
+        get => asset.devices;
+        set => asset.devices = value;
+    }
+
+    public ReadOnlyArray<InputControlScheme> controlSchemes => asset.controlSchemes;
+
+    public bool Contains(InputAction action)
+    {
+        return asset.Contains(action);
+    }
+
+    public IEnumerator<InputAction> GetEnumerator()
+    {
+        return asset.GetEnumerator();
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return GetEnumerator();
+    }
+
+    public void Enable()
+    {
+        asset.Enable();
+    }
+
+    public void Disable()
+    {
+        asset.Disable();
+    }
+
+    // Player
+    private readonly InputActionMap m_Player;
+    private IPlayerActions m_PlayerActionsCallbackInterface;
+    private readonly InputAction m_Player_Movement;
+    private readonly InputAction m_Player_Jump;
+    private readonly InputAction m_Player_Look;
+    private readonly InputAction m_Player_Rin;
+    public struct PlayerActions
+    {
+        private @Controller m_Wrapper;
+        public PlayerActions(@Controller wrapper) { m_Wrapper = wrapper; }
+        public InputAction @Movement => m_Wrapper.m_Player_Movement;
+        public InputAction @Jump => m_Wrapper.m_Player_Jump;
+        public InputAction @Look => m_Wrapper.m_Player_Look;
+        public InputAction @Rin => m_Wrapper.m_Player_Rin;
+        public InputActionMap Get() { return m_Wrapper.m_Player; }
+        public void Enable() { Get().Enable(); }
+        public void Disable() { Get().Disable(); }
+        public bool enabled => Get().enabled;
+        public static implicit operator InputActionMap(PlayerActions set) { return set.Get(); }
+        public void SetCallbacks(IPlayerActions instance)
+        {
+            if (m_Wrapper.m_PlayerActionsCallbackInterface != null)
+            {
+                @Movement.started -= m_Wrapper.m_PlayerActionsCallbackInterface.OnMovement;
+                @Movement.performed -= m_Wrapper.m_PlayerActionsCallbackInterface.OnMovement;
+                @Movement.canceled -= m_Wrapper.m_PlayerActionsCallbackInterface.OnMovement;
+                @Jump.started -= m_Wrapper.m_PlayerActionsCallbackInterface.OnJump;
+                @Jump.performed -= m_Wrapper.m_PlayerActionsCallbackInterface.OnJump;
+                @Jump.canceled -= m_Wrapper.m_PlayerActionsCallbackInterface.OnJump;
+                @Look.started -= m_Wrapper.m_PlayerActionsCallbackInterface.OnLook;
+                @Look.performed -= m_Wrapper.m_PlayerActionsCallbackInterface.OnLook;
+                @Look.canceled -= m_Wrapper.m_PlayerActionsCallbackInterface.OnLook;
+                @Rin.started -= m_Wrapper.m_PlayerActionsCallbackInterface.OnRin;
+                @Rin.performed -= m_Wrapper.m_PlayerActionsCallbackInterface.OnRin;
+                @Rin.canceled -= m_Wrapper.m_PlayerActionsCallbackInterface.OnRin;
+            }
+            m_Wrapper.m_PlayerActionsCallbackInterface = instance;
+            if (instance != null)
+            {
+                @Movement.started += instance.OnMovement;
+                @Movement.performed += instance.OnMovement;
+                @Movement.canceled += instance.OnMovement;
+                @Jump.started += instance.OnJump;
+                @Jump.performed += instance.OnJump;
+                @Jump.canceled += instance.OnJump;
+                @Look.started += instance.OnLook;
+                @Look.performed += instance.OnLook;
+                @Look.canceled += instance.OnLook;
+                @Rin.started += instance.OnRin;
+                @Rin.performed += instance.OnRin;
+                @Rin.canceled += instance.OnRin;
+            }
+        }
+    }
+    public PlayerActions @Player => new PlayerActions(this);
+    public interface IPlayerActions
+    {
+        void OnMovement(InputAction.CallbackContext context);
+        void OnJump(InputAction.CallbackContext context);
+        void OnLook(InputAction.CallbackContext context);
+        void OnRin(InputAction.CallbackContext context);
+    }
+}
---
+++ Assets/Abdullah/Scripts/PlayerController.cs
@@ -0,0 +1,117 @@
+﻿
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] public float gravity = -9.81f;
+    [SerializeField] public float jumpSpeed;
+
+    [SerializeField] public int walkSpeed;
+    [SerializeField] public int runSpeed;
+
+    [SerializeField] public float sensitivityX;
+    [SerializeField] public float sensitivityY;
+
+    [SerializeField] public float minY = -60f;
+    [SerializeField] public float maxY = 60f;
+
+    Vector3 jumped;
+
+    int moveSpeed;
+
+    CharacterController controller;
+    Vector3 moveDirection;
+    bool jump;
+    bool run;
+
+    Transform cam;
+    Transform player;
+
+    float lookX;
+    float lookY;
+
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        controller = GetComponent<CharacterController>();
+        player = transform;
+        cam = Camera.main.transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+        Look();
+        jumped.y -= gravity * Time.deltaTime;
+    }
+
+    void Jump() {
+
+        if (controller.isGrounded) {
+            jumped = new Vector3(0f,jumpSpeed,0f);
+        }       
+        
+    }
+
+
+    void Look()
+    {
+        lookY = Mathf.Clamp(lookY, minY, maxY);
+        player.localEulerAngles = new Vector3(0, lookX, 0);
+        cam.localEulerAngles = new Vector3(lookY, 0, 0);
+    }
+
+    void Move()
+    {
+        if (run)
+        {
+            moveSpeed = runSpeed;
+        }
+
+        if (!run)
+        {
+            moveSpeed = walkSpeed;
+        }
+
+        controller.Move(moveDirection * moveSpeed * Time.deltaTime);
+        controller.Move(jumped * Time.deltaTime);
+    }
+
+    
+    
+    public void OnMove(InputAction.CallbackContext context)
+    {
+        moveDirection = new Vector3(context.ReadValue<Vector2>().x, 0, context.ReadValue<Vector2>().y);
+        moveDirection = transform.TransformDirection(moveDirection);
+        
+    }
+
+    
+
+    public void OnLook(InputAction.CallbackContext context)
+    {
+        lookX += context.ReadValue<Vector2>().x * sensitivityX * Time.deltaTime;
+        lookY -= context.ReadValue<Vector2>().y * sensitivityY * Time.deltaTime;
+    }
+
+    public void OnJump(InputAction.CallbackContext context)
+    {
+        jump = context.performed;
+        if (jump)
+        {
+            Jump();
+        }
+    }
+
+    public void OnRun(InputAction.CallbackContext context)
+    {
+        run = context.performed;
+    }
+
+}
