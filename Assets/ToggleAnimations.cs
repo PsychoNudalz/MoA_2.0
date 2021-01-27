b93d67d2205e68ffc0ad7ff5a7fe8952b6ec1bf8
Daniel <Newsom>
2021-01-27 17:52:09 +0000

    Danny - Initial Enemy prefab animated

---
+++ Assets/ToggleAnimations.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+
+public class ToggleAnimations : MonoBehaviour
+{
+    Animator animator;
+    bool IsWalking;
+    bool IsDead;
+    Keyboard kb;
+
+    void Start()
+    {
+        kb = InputSystem.GetDevice<Keyboard>();
+        animator = GetComponent<Animator>();
+        IsWalking = animator.GetBool("IsWalking");
+    }
+
+    private void Update()
+    {
+        if (kb.wKey.wasPressedThisFrame)
+        {
+            IsWalking = !IsWalking;
+            animator.SetBool("IsWalking", IsWalking);
+        }
+
+        if (kb.dKey.wasPressedThisFrame)
+        {
+            animator.SetBool("IsDead", true);
+        }
+
+        if (kb.hKey.wasPressedThisFrame)
+        {
+            animator.SetTrigger("Hit");
+        }
+
+        if (kb.aKey.wasPressedThisFrame)
+        {
+            animator.SetTrigger("Attack");
+        }
+    }
+}
