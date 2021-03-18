774542ab79f6fda83d4087cb9dc2f5cb9ffd62e0
fceek <fceek96@gmail.com>
2021-03-18 02:43:26 +0000

    One scene, multiple rooms, randomise portal sequence.

---
+++ Assets/Chen/Scripts/Teleport/Portal.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Portal : MonoBehaviour
+{
+    public Portal portalTarget;
+
+    [SerializeField]
+    Transform targetSpawner;
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Setup() {
+        targetSpawner = portalTarget.transform.Find("SpawnPoint");
+    }
+    void OnTriggerEnter(Collider other) {
+        if (other.gameObject.CompareTag("Player")) {
+            Debug.Log("Ohhhhhhhhhhhhhhhhhhhhhhhhh");
+            GameObject player = GameObject.FindWithTag("Player");
+            player.SetActive(false);
+            player.transform.position = targetSpawner.transform.position;
+            player.SetActive(true);
+        }
+    }
+}
---
+++ Assets/Chen/Scripts/Teleport/TeleportManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TeleportManager : MonoBehaviour
+{
+    public List<Portal> portals = new List<Portal>();
+    public Portal start, end;
+    
+    void Start()
+    {
+        ShuffleList(portals);
+        Portal prev = start;
+        foreach (Portal pt in portals) {
+            prev.portalTarget = pt;
+            prev.Setup();
+            prev = pt;
+        }
+        prev.portalTarget = end;
+        prev.Setup();
+        end.GetComponent<BoxCollider>().enabled = false;
+    }
+
+    void ShuffleList(List<Portal> list) {
+        System.Random random = new System.Random();
+        int n = list.Count;
+
+        for (int i = n - 1; i > 1; i --) {
+            int rng = random.Next(i + 1);
+            Portal tmp = list[rng];
+            list[rng] = list[i];
+            list[i] = tmp;
+        }
+    }
+}
