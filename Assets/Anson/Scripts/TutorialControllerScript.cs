e23974beda34c6d37a8ad947d7afbea84d6679b9
Anson <anson.aw0618@gmail.com>
2021-04-04 00:53:58 +0100

    Anson: added more steps to tutorial

--- Assets/Anson/Scripts/TutorialControllerScript.cs
+++ Assets/Anson/Scripts/TutorialControllerScript.cs
@@ -5,14 +5,16 @@ using TMPro;
 
 public class TutorialControllerScript : MonoBehaviour
 {
-    [SerializeField] List<TextMeshProUGUI> allText;
-    [SerializeField] int textPointer = 0;
     [SerializeField] int stepValue = -1;
     [SerializeField] Animator animator;
     [SerializeField] GameObject door;
     [SerializeField] GunGeneratorScript gunGenerator;
     [SerializeField] Transform guns;
     [SerializeField] GameObject currentGun;
+    [SerializeField] ShootingRangeScript shootingRange;
+    [SerializeField] List<int> pickUpSteps ;
+    [SerializeField] List<int> killSteps;
+
 
     // Start is called before the first frame update
     void Start()
@@ -21,13 +23,29 @@ public class TutorialControllerScript : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        if (stepValue == 1&& currentGun != null)
+
+        //check for pick up on steps 1, 4
+        if (pickUpSteps.Contains(stepValue)&& currentGun != null)
         {
             if (!currentGun.transform.parent.Equals(guns))
             {
                 NextStep();
+                currentGun = null;
+
+            }
+        }
+
+
+        //check if player killed targets on steps 2, 3, 5
+        if (killSteps.Contains(stepValue))
+        {
+            if (shootingRange.IsWaveCleared())
+            {
+                NextStep();
+                shootingRange.ResetRange();
+                //print("WaveCleared");
             }
         }
     }
@@ -54,7 +72,31 @@ public class TutorialControllerScript : MonoBehaviour
 
     public void SpawnGun_PP()
     {
-        currentGun = gunGenerator.GenerateGun(0);
+        SpawnGun(0);
+    }
+    public void SpawnShootingRange(string seed, float waveTime, int numberOfWaves)
+    {
+        shootingRange.StartShootCourse(seed, waveTime, numberOfWaves);
+
+    }
+
+    public void SpawnShootingRange_Single()
+    {
+        shootingRange.StartShootCourse("01", float.PositiveInfinity,1);
+    }
+
+
+    public void SpawnShootingRange_Full()
+    {
+        shootingRange.StartShootCourse("111111111111", float.PositiveInfinity, 1);
+
+    }
+
+
+    public void SpawnGun(int i)
+    {
+        currentGun = null;
+        currentGun = gunGenerator.GenerateGun(i);
         currentGun.transform.SetParent(guns);
     }
 }
