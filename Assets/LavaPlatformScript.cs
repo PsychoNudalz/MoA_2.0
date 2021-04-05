ac46a25ecfa064d0bc4d3f73ec88cdf1b925001a
Daniel Newsom <danielgnewsom@gmail.com>
2021-04-05 17:00:31 +0100

    Danny - Add Lava platform movement

---
+++ Assets/LavaPlatformScript.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LavaPlatformScript : MonoBehaviour
+{
+    [SerializeField] GameObject lavaStart, lavaEnd;
+    [SerializeField] private float moveSpeed;
+    [SerializeField] private float rotationSpeed;
+    private Vector3 targetPosition;
+    private Vector3 startPosition;
+
+    private void Awake()
+    {
+        targetPosition = new Vector3(lavaEnd.transform.position.x, transform.position.y, transform.position.z);
+        startPosition = new Vector3(lavaStart.transform.position.x, transform.position.y, transform.position.z);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Vector3.Distance(targetPosition,transform.position) <= 1f)
+        {
+            transform.position = startPosition;
+        }
+        else
+        {
+            transform.position = Vector3.MoveTowards(transform.position,targetPosition, (moveSpeed / 100));
+            if(rotationSpeed >= 0)
+            {
+                transform.Rotate(new Vector3(0, 0, Random.Range(-0.1f,rotationSpeed)));
+            }
+            else
+            {
+                transform.Rotate(new Vector3(0, 0, Random.Range(rotationSpeed, 0.1f)));
+            }
+        }
+    }
+}
