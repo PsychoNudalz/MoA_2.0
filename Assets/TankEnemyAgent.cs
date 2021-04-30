fcab9ceeafc40e2713b781c85017307a5d371355
Daniel Newsom <danielgnewsom@gmail.com>
2021-04-30 12:57:38 +0100

    Danny - Fixes for enemy shooting

--- Assets/TankEnemyAgent.cs
+++ Assets/TankEnemyAgent.cs
@@ -34,7 +34,6 @@ public class TankEnemyAgent : MonoBehaviour
         TankEnemyAnimator = GetComponent<Animator>();
         ResetAttackTimer();
         gun = GetComponentInChildren<AIGunDamageScript>();
-        
     }
 
     /*
