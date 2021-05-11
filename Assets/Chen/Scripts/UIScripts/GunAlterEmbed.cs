67b3815eb91b8ed4a201015a2265c3699e91d87c
fceek <fceek96@gmail.com>
2021-05-11 11:48:14 +0100

    GunAlterMenu Debug mode and Help overlay

---
+++ Assets/Chen/Scripts/UIScripts/GunAlterEmbed.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class GunAlterEmbed : MonoBehaviour
+{
+    [SerializeField] bool isDebugBuild = true;
+    [SerializeField] Button debugBtn;
+    [SerializeField] GameObject debugMenu;
+    [SerializeField] GameObject helpOverlay;
+
+    void Start() {
+        if (!isDebugBuild) debugBtn.SetEnabled(false);
+    }
+    public void DebugButtonOnClick() {
+        debugMenu.SetActive(!debugMenu.activeSelf);
+    }
+
+    public void HelpButtonOnClick() {
+        helpOverlay.SetActive(true);
+    }
+
+    public void HelpCloseOnClick() {
+        helpOverlay.SetActive(false);
+    }
+}
