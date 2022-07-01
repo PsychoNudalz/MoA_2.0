41e6a4faf4804e8865691ac231c91050b59f8445
Anson <anson.aw0618@gmail.com>
2022-07-01 23:40:22 +0100

    projectile damage fixed

--- Assets/Anson/Scripts/Damage/SphereCastDamageScript.cs
+++ Assets/Anson/Scripts/Damage/SphereCastDamageScript.cs
@@ -4,32 +4,33 @@ using UnityEngine;
 
 public class SphereCastDamageScript : DamageScript
 {
-    [SerializeField] float lineOfSightOffset = 0f;
-    public bool SphereCastDamageArea(float dmg, float range, AnimationCurve rangeCurve, int level, ElementTypes elementType, bool needLineOfSight = false)
+    [SerializeField]
+    float lineOfSightOffset = 0f;
+
+    public bool SphereCastDamageArea(float dmg, float range, AnimationCurve rangeCurve, int level,
+        ElementTypes elementType, bool needLineOfSight = false)
     {
         if (elementType.Equals(ElementTypes.FIRE))
         {
             Debug.LogWarning("Something called Fire");
         }
-        
-        
+
+
         attackedTargets = new List<LifeSystemScript>();
         bool hitTarget = false;
         bool shockFlag = false;
         int calculatedDamage;
         RaycastHit[] hits = Physics.SphereCastAll(transform.position, range, transform.forward, range, layerMask);
+        LifeSystemScript lss;
         foreach (RaycastHit h in hits)
         {
             Collider c = h.collider;
-            //print("Check line of sight to:" + c.name);
-
-            if (!needLineOfSight || rayCastLineOfSight(c, range))
+            if (!needLineOfSight || RayCastLineOfSight(c, range))
             {
+                 lss = c.GetComponentInParent<LifeSystemScript>();
 
-
-                if (tagList.Contains(c.tag) && c.GetComponentInParent<LifeSystemScript>() != null)
+                if (tagList.Contains(c.tag) && lss)
                 {
-                    LifeSystemScript lss = c.GetComponentInParent<LifeSystemScript>();
                     if (!attackedTargets.Contains(lss))
                     {
                         hitTarget = true;
@@ -40,7 +41,6 @@ public class SphereCastDamageScript : DamageScript
 
                         if (!(lss is PlayerLifeSystemScript))
                         {
-
                             if (elementType.Equals(ElementTypes.SHOCK))
                             {
                                 if (!shockFlag)
@@ -52,7 +52,6 @@ public class SphereCastDamageScript : DamageScript
                             else
                             {
                                 ApplyElementEffect(lss, calculatedDamage * .5f, range, elementType);
-
                             }
                         }
                     }
@@ -69,21 +68,25 @@ public class SphereCastDamageScript : DamageScript
         return Mathf.RoundToInt(dmg * rangeCurve.Evaluate((pos - transform.position).magnitude / range)) + 1;
     }
 
-    bool rayCastLineOfSight(Collider c, float range)
+    bool RayCastLineOfSight(Collider c, float range)
     {
-        Vector3 targetOffset = new Vector3(0, 0.5f, 0);
-        Vector3 dir = (c.transform.position+targetOffset - transform.position).normalized;
+        Vector3 dir = (c.transform.position - transform.position).normalized;
         RaycastHit hit;
-        Vector3 offset = dir * lineOfSightOffset;
-        Debug.DrawRay(transform.position+offset, dir * range, Color.blue, 3f);
         if (Physics.Raycast(transform.position, dir, out hit, range, layerMask))
         {
+            Debug.DrawRay(transform.position, dir * range, Color.blue, 3f);
+
             //print("Check line of sight found:" + hit.collider.name);
             if (hit.collider.Equals(c))
             {
                 //print("have line of sight");
                 return true;
             }
+        }
+        else
+        {
+            Debug.DrawRay(transform.position, dir * range, Color.red, 3f);
+
         }
         //print("no line of sight");
 
